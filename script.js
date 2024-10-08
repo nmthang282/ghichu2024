3ab13bab570e8572a43f778bc67ac55443a28eb3
nmthang282 <85443250+nmthang282@users.noreply.github.com>
2024-10-08 15:06:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Cấu hình Firebase
-var firebaseConfig = {
-  apiKey: "AIzaSyD9ozJ6k-2DVdrhwbO4EdGsYLGiW3sjObY",
-  authDomain: "ghichu2024-54519.firebaseapp.com",
-  databaseURL: "https://ghichu2024-54519-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ghichu2024-54519",
-  storageBucket: "ghichu2024-54519.appspot.com",
-  messagingSenderId: "276303946107",
-  appId: "1:276303946107:web:11ffb6dd550d7733a49dde",
-  measurementId: "G-XFJRPLM4Z3"
-};
+//var firebaseConfig = {
+ // apiKey: "AIzaSyD9ozJ6k-2DVdrhwbO4EdGsYLGiW3sjObY",
+//  authDomain: "ghichu2024-54519.firebaseapp.com",
+//  databaseURL: "https://ghichu2024-54519-default-rtdb.asia-southeast1.firebasedatabase.app",
+//projectId: "ghichu2024-54519",
+//  storageBucket: "ghichu2024-54519.appspot.com",
+//messagingSenderId: "276303946107",
+//  appId: "1:276303946107:web:11ffb6dd550d7733a49dde",
+//  measurementId: "G-XFJRPLM4Z3"
+//};
 
 // Khởi tạo Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+//firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
 
