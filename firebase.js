3c882a7375b4f155e36ea2823f79dbd8b0029922
nmthang282 <85443250+nmthang282@users.noreply.github.com>
2024-10-08 14:05:19 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Tệp firebase.js
+
+// Firebase configuration (lấy từ Firebase Console)
+ const firebaseConfig = {
+    apiKey: "AIzaSyD9ozJ6k-2DVdrhwbO4EdGsYLGiW3sjObY",
+    authDomain: "ghichu2024-54519.firebaseapp.com",
+    databaseURL: "https://ghichu2024-54519-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ghichu2024-54519",
+    storageBucket: "ghichu2024-54519.appspot.com",
+    messagingSenderId: "276303946107",
+    appId: "1:276303946107:web:11ffb6dd550d7733a49dde",
+    measurementId: "G-XFJRPLM4Z3"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Kiểm tra trạng thái đăng nhập
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // Người dùng đã đăng nhập
+    console.log("User is logged in:", user.email);
+  } else {
+    // Người dùng chưa đăng nhập, chuyển hướng đến trang đăng nhập
+    window.location.href = "login.html";
+  }
+});
