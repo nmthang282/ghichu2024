11d41a053d4e494342d92d5e9cadb12f561bfa1f
nmthang282 <85443250+nmthang282@users.noreply.github.com>
2024-10-08 16:01:34 +0700

    Update login.js
    fix log in

--- login.js
+++ login.js
@@ -1,19 +1,14 @@
-
-
-// Hàm đăng nhập
+// login.js
 function login() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    auth.signInWithEmailAndPassword(email, password)
+    firebase.auth().signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            // Đăng nhập thành công
-            alert('Đăng nhập thành công!');
-            // Chuyển hướng đến trang ghi chú
-            window.location.href = 'index.html'; // Thay bằng tên tệp ghi chú của bạn
+            // Đăng nhập thành công, chuyển hướng về trang ghi chú
+            window.location.href = 'index.html';
         })
         .catch((error) => {
-            // Xử lý lỗi
             alert(error.message);
         });
 }
